179d3e84ec1eedbcf8e479bb55627a29df5a39f2
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-30 16:48:03 +0100

    Mappati i due modelli Type e Project: - Type has many projects - Project belongs to Type

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -10,4 +10,8 @@ class Project extends Model
     use HasFactory;
 
     protected $guarded = ['slug', 'cover_image'];
+
+    public function type() {
+        return $this->belongsTo(Type::class);
+    }
 }
