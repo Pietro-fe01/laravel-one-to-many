f428434da4bc38c6b0874c2e979e426e8f0dba26
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2023-01-30 15:04:10 +0100

    Popolato la tabella Types con 12 tipi di progetti diversi tramite il seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +16,29 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Refresh records quando viene invocato il seeder tramite CLI
+        Type::truncate();
+
+        $types = [
+            'Business implementation',
+            'Foundational (business improvement)',
+            'IT infrastructure improvement',
+            'Product development (IT)',
+            'Product development (non-IT)',
+            'Physical engineering/construction',
+            'Physical infrastructure improvement',
+            'Procurement',
+            'Regulatory/compliance',
+            'Research and Development (R&D)',
+            'Service development',
+            'Transformation/reengineering'
+        ];
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type->name = $type;
+            $new_type->slug = Str::of($new_type->name)->slug('-');
+            $new_type->save();
+        }
     }
 }
